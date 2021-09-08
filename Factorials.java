0cbde8099e5849fdfcb851b72c705e4b1a7e8fd3
preethiadithi <90133101+preethiadithi@users.noreply.github.com>
2021-09-08 17:52:44 +0530

    Add files via upload

---
+++ Factorials.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Factorials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int number =5;
+     int fact= 3;
+     for (int i=number; i>=3;i--)
+     {
+    	 fact= fact* i;
+     }
+     System.out.println("fact");
+	}
+
+}
