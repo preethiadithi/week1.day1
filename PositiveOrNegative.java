6b6be589820042c514d6abb6d4c6ffb30949aecf
preethiadithi <90133101+preethiadithi@users.noreply.github.com>
2021-09-08 17:53:08 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		int number=35;
+		if(number>0)
+			System.out.println("positive");
+		else if(number<0)
+			System.out.println("negative");
+		else
+			System.out.println("zero");
+		// TODO Auto-generated method stub
+
+	}
+
+}
